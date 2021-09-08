46a8d90bee433110f4017f44159577fc38bc4b8f
Tyler Shelton <bullfrogdevstudio@gmail.com>
2021-09-07 22:57:28 -0400

    Complete code along with Eric to get the pokedex connected and up and running

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 //PART I D.O.M. Manipulation
 
 // Select the search button pokeball by its HTML Id Attribute
-var searchBtn = document.getElementById('ADD CODE'); // search button pokeball
+var searchBtn = document.getElementById('search-btn'); // search button pokeball
 
 // Select the search field input by its HTML Id Attribute
-var inputField = document.getElementById('ADD CODE'); // search field input
+var inputField = document.getElementById('name-input'); // search field input
 
 // Select the pokemon name screen by it's HTML Id Attribute
-var nameScreen = document.getElementById('ADD CODE'); //pokemon name-screen
+var nameScreen = document.getElementById('name-screen'); //pokemon name-screen
 
 // Select the pokemon image screen by it's HTML Id Attribute
-var imageScreen = document.getElementById('ADD CODE'); // pokemon image screen
+var imageScreen = document.getElementById('main-screen'); // pokemon image screen
 
 // Select the height and weight screen by it's HTML Id Attribute
-var aboutScreen = document.getElementById('ADD CODE'); // about-text screen
+var aboutScreen = document.getElementById('about-screen'); // about-text screen
 
 // Select the type screen by it's HTML Id Attribute
-var typeScreen = document.getElementById('ADD CODE'); // pokemon type screen
+var typeScreen = document.getElementById('type-screen'); // pokemon type screen
 
 // Select the pokemon id number screen by it's HTML Id Attribute
-var idScreen = document.getElementById('ADD CODE'); // pokemon id number screen
+var idScreen = document.getElementById('id-screen'); // pokemon id number screen
 
 //PART II Fetching the Pokemon Data from the PokeAPI
 
@@ -35,10 +35,12 @@ var getPokemonData = (pokemon) => {
     .then((response) => response.json())
     //a callback function to take the data from out newly converted json data and insert it into our appropriate DOM elements
     .then((data) => {
+      console.log(pokemon);
+      console.log('DATA FROM POKEAPI: ' + data);
       //create a format appropriate pokemon id in order to plug into the pokemon.com image url
       var id = ('00' + data.id).slice(-3);
       //change the background image of the image screen element to the appropriate pokemon image
-      imageScreen.style.backgroundImage = `url('ADD CODE')`;
+      imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
       //change the text of the name screen element to the appropriate pokemon name
       nameScreen.innerHTML = data.name;
       //change the text of the type screen element to the appropriate pokemon type
@@ -46,9 +48,8 @@ var getPokemonData = (pokemon) => {
       //change the text of the id screen element to the appropriate pokemon id
       idScreen.innerHTML = `#${data.id}`;
       //change the text of the id screen element to the appropriate pokemon height and weight
-      aboutScreen.innerHTML = `Height: ${data.height * 10}cm Weight: ${
-        data.weight / 10
-      }kg`;
+      aboutScreen.innerHTML = `Height: ${data.height * 10}cm Weight: ${data.weight / 10
+        }kg`;
       //clear the input field in order for the user to input another pokemon name or id
       inputField.value = '';
     })
@@ -59,7 +60,22 @@ var getPokemonData = (pokemon) => {
 //Create an event listner that attaches to the search button poke ball and activates when a user 'clicks' on it!
 //The event listener should call our pokemon data function from PART II
 
-searchBtn.addEventListener('click', () => getPokemonData("ADD CODE"));
+searchBtn.addEventListener('click', () => getPokemonData(inputField.value));
+// searchBtn.addEventListener('keyup', (event) => {
+//   if (event.keyCode === 13) {
+//     event.preventDefault();
+//   }
+//   //document.getElementById('search-btn')
+//   getPokemonData(inputField.value);
+// })
+
+// searchBtn.addEventListener('keyup', (event) => {
+//   if (event.keycode === 13) {
+//     event.preventDefault();
+//   }
+//   document.getElementById('search-btn').click();
+//   getPokemonData(inputField.value);
+// });
 
 
 // <!-- Copyright (c) 2021 by oryam (https://codepen.io/oryamne/pen/vYKXbgZ)
